37aa8e252b2edc43a0c971da1632e3170d2ba8a2
Eser Ozvataf <eser@ozvataf.com>
2018-11-10 20:48:14 +0300

    Refined unit tests.

--- src/lib/__tests__/assign.ts
+++ src/lib/__tests__/assign.ts
@@ -1,29 +1,31 @@
 import assign, { assignPolyfill } from '../assign';
 
-test('having the original assign if available', () => {
-    if (Object.hasOwnProperty('assign')) {
-        expect(Object['assign']).toBe(assign);
-    }
-});
+describe('assign', () => {
+    test('having the original assign if available', () => {
+        if (Object.hasOwnProperty('assign')) {
+            expect(Object['assign']).toBe(assign);
+        }
+    });
 
-test('ensure skipping null/undefined sources', () => {
-    const obj1 = {};
-    const obj2: any = null;
-    const obj3: any = undefined;
+    test('ensure skipping null/undefined sources', () => {
+        const obj1 = {};
+        const obj2: any = null;
+        const obj3: any = undefined;
 
-    const result = assignPolyfill(obj1, obj2, obj3);
+        const result = assignPolyfill(obj1, obj2, obj3);
 
-    expect(result).toBe(obj1);
-    expect(result).toEqual(obj1);
-});
+        expect(result).toBe(obj1);
+        expect(result).toEqual(obj1);
+    });
 
-test('assign values right to left', () => {
-    const obj1 = {};
-    const obj2 = { test: true };
+    test('assign values right to left', () => {
+        const obj1 = {};
+        const obj2 = { test: true };
 
-    const result = assignPolyfill(obj1, obj2);
+        const result = assignPolyfill(obj1, obj2);
 
-    expect(result).toBe(obj1);
-    expect(result).not.toBe(obj2);
-    expect(result).toEqual({ test: true });
+        expect(result).toBe(obj1);
+        expect(result).not.toBe(obj2);
+        expect(result).toEqual({ test: true });
+    });
 });
