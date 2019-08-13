816a69a3f4b3f4ee6c2a5c2cb02212b4f7fb9b9b
Eser Ozvataf <eser@ozvataf.com>
2019-08-13 23:39:59 +0300

    Updated dependencies and bumped to version 0.13.2.

---
+++ dist/index.umd.js
@@ -0,0 +1,168 @@
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+    typeof define === 'function' && define.amd ? define(['exports'], factory) :
+    (global = global || self, factory(global.ponyfills = {}));
+}(this, function (exports) { 'use strict';
+
+    /*! *****************************************************************************
+    Copyright (c) Microsoft Corporation. All rights reserved.
+    Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+    this file except in compliance with the License. You may obtain a copy of the
+    License at http://www.apache.org/licenses/LICENSE-2.0
+
+    THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+    WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+    MERCHANTABLITY OR NON-INFRINGEMENT.
+
+    See the Apache Version 2.0 License for specific language governing permissions
+    and limitations under the License.
+    ***************************************************************************** */
+
+    function __values(o) {
+        var m = typeof Symbol === "function" && o[Symbol.iterator], i = 0;
+        if (m) return m.call(o);
+        return {
+            next: function () {
+                if (o && i >= o.length) o = void 0;
+                return { value: o && o[i++], done: !o };
+            }
+        };
+    }
+
+    function __read(o, n) {
+        var m = typeof Symbol === "function" && o[Symbol.iterator];
+        if (!m) return o;
+        var i = m.call(o), r, ar = [], e;
+        try {
+            while ((n === void 0 || n-- > 0) && !(r = i.next()).done) ar.push(r.value);
+        }
+        catch (error) { e = { error: error }; }
+        finally {
+            try {
+                if (r && !r.done && (m = i["return"])) m.call(i);
+            }
+            finally { if (e) throw e.error; }
+        }
+        return ar;
+    }
+
+    function __spread() {
+        for (var ar = [], i = 0; i < arguments.length; i++)
+            ar = ar.concat(__read(arguments[i]));
+        return ar;
+    }
+
+    function arrayFromPolyfill(source, mapFn, thisArg) {
+        var caller = (thisArg === null || thisArg === undefined) ? this : thisArg;
+        if (source === null || source === undefined) {
+            throw new TypeError('Array.from requires an array-like object - not null or undefined');
+        }
+        var mapping = (mapFn !== null && mapFn !== undefined);
+        if (mapping && !(mapFn instanceof Function)) {
+            throw new TypeError('Array.from: when provided, the second argument must be a function');
+        }
+        var items = __spread(source);
+        if (!mapping) {
+            return items;
+        }
+        var result = items.reduce(function (prev, curr, idx) { return (__spread(prev, [mapFn.call(caller, curr, idx)])); }, []);
+        return result;
+    }
+    var arrayFrom = (Array.hasOwnProperty('from') ? Array.from : arrayFromPolyfill);
+
+    function objectAssignPolyfill(target) {
+        var e_1, _a, e_2, _b;
+        var sources = [];
+        for (var _i = 1; _i < arguments.length; _i++) {
+            sources[_i - 1] = arguments[_i];
+        }
+        try {
+            for (var sources_1 = __values(sources), sources_1_1 = sources_1.next(); !sources_1_1.done; sources_1_1 = sources_1.next()) {
+                var source = sources_1_1.value;
+                if (source === null || source === undefined) {
+                    continue;
+                }
+                try {
+                    for (var _c = (e_2 = void 0, __values(Object.getOwnPropertyNames(source))), _d = _c.next(); !_d.done; _d = _c.next()) {
+                        var key = _d.value;
+                        target[key] = source[key];
+                    }
+                }
+                catch (e_2_1) { e_2 = { error: e_2_1 }; }
+                finally {
+                    try {
+                        if (_d && !_d.done && (_b = _c.return)) _b.call(_c);
+                    }
+                    finally { if (e_2) throw e_2.error; }
+                }
+            }
+        }
+        catch (e_1_1) { e_1 = { error: e_1_1 }; }
+        finally {
+            try {
+                if (sources_1_1 && !sources_1_1.done && (_a = sources_1.return)) _a.call(sources_1);
+            }
+            finally { if (e_1) throw e_1.error; }
+        }
+        return target;
+    }
+    var objectAssign = (Object.hasOwnProperty('assign') ? Object.assign : objectAssignPolyfill);
+
+    function reflectOwnKeysPolyfill(source) {
+        return __spread(Object.getOwnPropertyNames(source), Object.getOwnPropertySymbols(source));
+    }
+    var reflectOwnKeys = (Reflect.hasOwnProperty('ownKeys') ? Reflect.ownKeys : reflectOwnKeysPolyfill);
+
+    function objectEntriesPolyfill(source) {
+        var keys = reflectOwnKeys(source);
+        var result = keys.reduce(function (prev, curr) {
+            if (curr.constructor === String && source.propertyIsEnumerable(curr)) {
+                return __spread(prev, [[curr, source[curr]]]);
+            }
+            return prev;
+        }, []);
+        return result;
+    }
+    var objectEntries = (Object.hasOwnProperty('entries') ? Object.entries : objectEntriesPolyfill);
+
+    function objectValuesPolyfill(source) {
+        var keys = reflectOwnKeys(source);
+        var result = keys.reduce(function (prev, curr) {
+            if (curr.constructor === String && source.propertyIsEnumerable(curr)) {
+                return __spread(prev, [source[curr]]);
+            }
+            return prev;
+        }, []);
+        return result;
+    }
+    var objectValues = (Object.hasOwnProperty('values') ? Object.values : objectValuesPolyfill);
+
+    var library = {
+        arrayFrom: arrayFrom,
+        arrayFromPolyfill: arrayFromPolyfill,
+        objectAssign: objectAssign,
+        objectAssignPolyfill: objectAssignPolyfill,
+        objectEntries: objectEntries,
+        objectEntriesPolyfill: objectEntriesPolyfill,
+        objectValues: objectValues,
+        objectValuesPolyfill: objectValuesPolyfill,
+        reflectOwnKeys: reflectOwnKeys,
+        reflectOwnKeysPolyfill: reflectOwnKeysPolyfill,
+    };
+
+    exports.arrayFrom = arrayFrom;
+    exports.arrayFromPolyfill = arrayFromPolyfill;
+    exports.default = library;
+    exports.objectAssign = objectAssign;
+    exports.objectAssignPolyfill = objectAssignPolyfill;
+    exports.objectEntries = objectEntries;
+    exports.objectEntriesPolyfill = objectEntriesPolyfill;
+    exports.objectValues = objectValues;
+    exports.objectValuesPolyfill = objectValuesPolyfill;
+    exports.reflectOwnKeys = reflectOwnKeys;
+    exports.reflectOwnKeysPolyfill = reflectOwnKeysPolyfill;
+
+    Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,54 @@
 import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
+import cleanup from 'rollup-plugin-cleanup';
+import pkg from './package.json';
 
-export default {
-    input: './src/index.ts',
-    output: [
-        {
-            file: './dist/index.min.js',
-            format: 'umd',
-            exports: 'named',
-            name: 'ponyfills',
-        },
-        {
-            file: './dist/index.esm.min.js',
-            format: 'es',
-        },
-    ],
-    external: [],
-    plugins: [
-        typescript({
-            tsconfigOverride: {
-                compilerOptions: {
-                    module: 'es2015',
-                },
+export default [
+    {
+        input: './src/index.ts',
+        output: [
+            // browser-friendly UMD build
+            {
+                file: './dist/index.umd.js',
+                format: 'umd',
+                exports: 'named',
+                name: pkg.name,
             },
-        }),
-        terser({
-            include: [ /^.+\.min\.js$/ ],
-        }),
-    ],
-};
+            // browser-friendly UMD build (minified)
+            {
+                file: './dist/index.umd.min.js',
+                format: 'umd',
+                exports: 'named',
+                name: pkg.name,
+            },
+            // ES module
+            {
+                file: './dist/index.esm.js',
+                format: 'es',
+            },
+            // ES module (minified)
+            {
+                file: './dist/index.esm.min.js',
+                format: 'es',
+            },
+        ],
+        external: Object.keys(pkg.dependencies),
+        plugins: [
+            typescript({
+                tsconfigOverride: {
+                    compilerOptions: {
+                        module: 'es2015',
+                    },
+                },
+            }),
+            cleanup({
+                comments: 'none',
+            }),
+            terser({
+                include: [ /^.+\.min\.js$/ ],
+            }),
+            sizeSnapshot(),
+        ],
+    },
+];
